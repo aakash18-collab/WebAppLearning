a5eda78607f2efe1b04f20bb41292b21655b6f18
Aakash Dahal <76913077+aakash18-collab@users.noreply.github.com>
2022-06-15 23:24:57 +0545

    Added a new MVC project and controller and view were created.

---
+++ MvcWebAppStudent/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcStudent.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        ////This is default method the controller will be calling
+        //public string Index()
+        //{
+        //    return "Hello World";
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //this welcome method should be called manually by us in URL
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" +" "+ name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+      
+    }
+}
